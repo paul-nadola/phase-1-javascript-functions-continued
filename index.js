ed99ea6de525a6b7eceae01388292700109dcee1
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-14 15:51:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+//let activity = 'roller-skate'
+function saturdayFun (activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(out='*'){
+    return function( par = 'special'){
+        return `You are ${out}${par}${out}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer")
